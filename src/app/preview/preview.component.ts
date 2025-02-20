429b011771f2faac3cb573e829e36b4297d3736e
Dayanithi_E_K <dayanithiek.21cse@kongu.edu>
2025-02-20 14:09:43 +0530

    response page completed

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,35 +1,56 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SurveyCardComponent } from "../survey-card/survey-card.component";
+import { SurveyCardComponent } from '../survey-card/survey-card.component';
 import { FetchService } from '../services/fetch/fetch.service';
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-preview',
   standalone: true,
-  imports: [CommonModule, SurveyCardComponent],
+  imports: [CommonModule, SurveyCardComponent, ReactiveFormsModule],
   templateUrl: './preview.component.html',
-  styleUrl: './preview.component.css'
+  styleUrl: './preview.component.css',
 })
 export class PreviewComponent {
-  
-  surveyData !: any;
-  constructor(private fetchService: FetchService, private router: Router) {}
+  surveyData: any;
+  surveyForm: any;
+
+  constructor(
+    private fetchService: FetchService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.surveyData = history.state.survey;
     this.surveyData.questions.forEach((question: any, index: number) => {
       question.question_num = index + 1;
     });
-    console.log(this.surveyData);
+
+    let formControls: { [key: string]: FormControl } = {};
+
+    this.surveyData.questions.forEach((q: any) => {
+      if (q.inputType === 'checkbox') {
+        formControls['question' + q.question_num] = new FormControl([]);
+      } else {
+        formControls['question' + q.question_num] = new FormControl('');
+      }
+    });
+
+    this.surveyForm = this.formBuilder.group(formControls);
+
   }
 
-  createSurvey(){
+  getFormControl(index: string): FormControl {
+    return this.surveyForm.controls[index] as FormControl;
+  }
+
+  createSurvey() {
     console.log(this.surveyData);
     this.fetchService.createSurvey(this.surveyData).subscribe((data: any) => {
       console.log(data);
       this.router.navigate(['admin/']);
-    })
+    });
   }
-
 }
--- src/app/response/response.component.ts
+++ src/app/response/response.component.ts
@@ -1,45 +1,97 @@
 import { Component, Input } from '@angular/core';
 import { FetchService } from '../services/fetch/fetch.service';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-response',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './response.component.html',
-  styleUrl: './response.component.css'
+  styleUrl: './response.component.css',
 })
 export class ResponseComponent {
   @Input() id!: string;
   page: number = 0;
   size: number = 5;
-  from: string = '';
-  to: string = '';
-  responses: any;
-  surveyData: any; 
+  minToDate: string = '';
+  responseData: any;
+  surveyData: any;
+  dateForm!: FormGroup;
 
-  constructor(private fetchService: FetchService) {}
+  constructor(private fetchService: FetchService, private formBuilder: FormBuilder) {
+    this.dateForm = this.formBuilder.group({
+      fromDate: [''],
+      toDate: ['']
+    });
+  }
 
   ngOnInit() {
     this.surveyData = history.state.survey;
-    this.fetchService.getResponsesOfSurvey(this.id, this.page, this.size, this.from, this.to).subscribe((data: any) => {
-      this.responses = data;
-      console.log(this.responses)
-    })
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.fetchService
+      .getResponsesOfSurvey(this.id, this.page, this.size, this.dateForm.get('fromDate')?.value, this.dateForm.get('toDate')?.value)
+      .subscribe((data: any) => {
+        this.responseData = data;
+      });
   }
 
   objectKeys(obj: any): string[] {
     return Object.keys(obj);
   }
-  
+
   formatResponse(value: any): string {
     if (Array.isArray(value)) {
-      return value.join(', '); 
+      return value.join(', ');
     } else if (typeof value === 'object' && value !== null) {
       return '-';
     }
     return value;
   }
-  
 
+  onSizeChange(event: Event) {
+    this.size = (event.target as HTMLSelectElement).value as unknown as number;
+    this.fetchData();
+  }
+
+  incrementPage() {
+    if (!this.responseData.last) {
+      this.page++;
+      this.fetchData();
+    }
+  }
+
+  decrementPage() {
+    if (!this.responseData.first) {
+      this.page--;
+      this.fetchData();
+    }
+  }
+
+  updateMinDate() {
+    const from = this.dateForm.get('fromDate')?.value;
+    if(from){
+      this.minToDate = from;
+      this.dateForm.get('toDate')?.setValue('');
+    }
+  }
+
+  onSearch() {
+    this.page = 0;
+    if(this.dateForm.valid && this.dateForm.value.fromDate && this.dateForm.value.toDate) {
+      this.fetchData();
+    }
+    
+  }
+  
+  onReset() {
+    this.dateForm.get('toDate')?.setValue('');
+    this.dateForm.get('fromDate')?.setValue('');
+    this.minToDate = '';
+    this.page = 0;
+    this.fetchData();
+  }
 }
